ca0845c6ba969ec767ee86510927e48432767a6c
IShaLinI <mrsaltinecracker1@gmail.com>
2024-01-14 16:13:25 -0800

    Webhook Test

--- src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
@@ -1,14 +1,6 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import frc.robot.Constants.CANID;
-
-// falcon motors
-
 public class IntakeIO_Real implements IntakeIO {
-  private TalonFX OuttakeWeelsMotor = new TalonFX(CANID.kOutakeWeels);
-
   public IntakeIO_Real() {
-    // motor configureation stuff here
   }
 }
--- src/main/java/frc/robot/subsystems/outtake/OuttakeIO_Real.java
+++ src/main/java/frc/robot/subsystems/outtake/OuttakeIO_Real.java
@@ -1,13 +1,5 @@
 package frc.robot.subsystems.outtake;
-
-import com.ctre.phoenix6.hardware.TalonFX;
-import frc.robot.Constants.CANID;
-
-// falcon motors
-
 public class OuttakeIO_Real implements OuttakeIO {
-  private TalonFX OuttakeWeelsMotor = new TalonFX(CANID.kOutakeWeels);
-
   public OuttakeIO_Real() {
     // motor configureation stuff here
   }
