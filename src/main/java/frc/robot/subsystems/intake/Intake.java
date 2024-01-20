5eee09cd90d32da2abd3dea597e8ba56c49b9921
areardon3 <aereardon06@gmail.com>
2024-01-20 09:20:38 -0800

    Deleted mystery intake code

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
@@ -14,28 +13,8 @@ public class Intake extends SubsystemBase {
   private final IntakeIO intakeIO;
   private final IntakeIOInputsAutoLogged intakeInputs = new IntakeIOInputsAutoLogged();
 
-  private double rpmSetpoint = 0.0;
-
-  public Intake(IntakeIO outtakeIO) {
-    this.intakeIO = outtakeIO;
-  }
-
-  public Command run() {
-    return this.run(
-        () -> {
-          intakeIO.runRollers(rpmSetpoint);
-        });
-  }
-
-  public Command changeRPM(double rpm) {
-    return this.runOnce(
-        () -> {
-          rpmSetpoint = rpm;
-        });
-  }
-
-  public void setAngle(double angle) {
-    intakeIO.runPivot(angle);
+  public Intake(IntakeIO intakeIO) {
+    this.intakeIO = intakeIO;
   }
 
   @Override
--- src/main/java/frc/robot/subsystems/intake/IntakeIO_Sim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO_Sim.java
@@ -1,46 +1,5 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import frc.robot.Constants.CodeConstants;
-
 public class IntakeIO_Sim implements IntakeIO {
-  private double voltage = 0;
-  private double angle = 0;
-
-  private FlywheelSim flywheelSim = new FlywheelSim(DCMotor.getFalcon500(2), 2, 0.00146376);
-
-  private SimpleMotorFeedforward flyWheelFeedForward =
-      new SimpleMotorFeedforward(0.1, 0.00374064837); // 0.003639801
-  private PIDController flyWheelPID = new PIDController(0.0032, 0, 0);
-
-  @Override
-  public void updateInputs(IntakeIOInputs inputs) {
-
-    flywheelSim.update(1 / CodeConstants.kMainLoopFrequency);
-
-    inputs.currentRPM = flywheelSim.getAngularVelocityRPM();
-    inputs.rollerMotorVoltage = voltage;
-    inputs.rollerMotorTemp = 0;
-    inputs.rollerMotorCurrent = flywheelSim.getCurrentDrawAmps();
-
-    inputs.pivotMotorPosition = angle;
-  }
-
-  @Override
-  public void runRollers(double rpm) {
-    rpm = MathUtil.clamp(rpm, -3190, 3190);
-    double ffEffort = flyWheelFeedForward.calculate(rpm);
-    double pidEffort = flyWheelPID.calculate(flywheelSim.getAngularVelocityRPM(), rpm);
-    voltage = MathUtil.clamp(ffEffort + pidEffort, -12, 12);
-    flywheelSim.setInput(voltage);
-  }
-
-  @Override
-  public void runPivot(double angle) {
-    this.angle = angle;
-  }
+  public IntakeIO_Sim() {}
 }
